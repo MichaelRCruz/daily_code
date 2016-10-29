53dbffe74a753627a513d8f4c0e5ae136ec6f69b
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-29 16:44:33 -0700

    seeds development data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  { :name => 'Michael', :email => 'michael@michael.com', :password_digest => '1234', :admin => true },
+  { :name => 'Emily', :email => 'emily@emily.com', :password_digest => '1234', :admin => false }
+  ])
+
+posts = Post.create([
+  { :title => 'Michael Blog Title', :snippet => 'script.js', :language => 'javascript', :content => 'This was my thought process.' },
+  { :title => 'Emily Blog Title', :snippet => 'ruby.rb', :language => 'ruby', :content => 'How should I refactor' }
+  ])
+
+comments = Comment.create([
+  { :content => 'Cool idea!' },
+  { :content => 'Refactor with gusto!' }
+  ])
