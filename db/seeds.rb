2238bc7292811d090fcae1bdd3eb6b5147f8444b
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-29 20:33:04 -0700

    fixes seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# users = User.create([
+#   { :name => 'Michael', :email => 'michael@michael.com', :password_digest => '1234', :admin => true },
+#   { :name => 'Emily', :email => 'emily@emily.com', :password_digest => '1234', :admin => false }
+#   ])
+
 users = User.create([
-  { :name => 'Michael', :email => 'michael@michael.com', :password_digest => '1234', :admin => true },
-  { :name => 'Emily', :email => 'emily@emily.com', :password_digest => '1234', :admin => false }
+  { :name => 'Michael', :email => 'michael@michael.com', :password => '1234', :admin => true },
+  { :name => 'Emily', :email => 'emily@emily.com', :password => '1234', :admin => false }
   ])
 
 posts = Post.create([
-  { :title => 'Michael Blog Title', :snippet => 'script.js', :language => 'javascript', :content => 'This was my thought process.' },
-  { :title => 'Emily Blog Title', :snippet => 'ruby.rb', :language => 'ruby', :content => 'How should I refactor' }
+  { :title => 'Michael Blog Title', :snippet => 'script.js', :language => 'javascript', :content => 'This was my thought process.', :user => users[0] },
+  { :title => 'Emily Blog Title', :snippet => 'ruby.rb', :language => 'ruby', :content => 'How should I refactor', :user => users[1] }
   ])
 
 comments = Comment.create([
-  { :content => 'Cool idea!' },
-  { :content => 'Refactor with gusto!' }
+  { :content => 'Cool idea!', :user => users[0], :post => posts[0] },
+  { :content => 'Refactor with gusto!', :user => users[1], :post => posts[1] }
   ])
