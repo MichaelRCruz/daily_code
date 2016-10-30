e193ad8ecdc0f48193430d6eb2eb6fb8a737341a
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-30 14:08:48 -0700

    adds to .gitignore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:login][:email])
     if user && user.authenticate(params[:login][:password])
       session[:user_id] = user.id.to_s
-      redirect_to users_path
+      redirect_to posts_path
     else
       render :new
     end
