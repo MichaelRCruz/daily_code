d9fbfd56d973817df8d543dccbfbda0d65b7affc
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-29 21:57:54 -0700

    adds user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   get 'users/' => 'users#index'
   get 'users/new' => 'users#new', as: :new_user
   get 'users/:id' => 'users#show', as: :user
