d9825f2ee0f39a51f2438203ab015e32c5e69f96
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-30 23:24:39 -0700

    fixes login problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:login][:email])
-    if user && user.authenticate(params[:login][:password][:password_confirmation])
+    if user && user.authenticate(params[:login][:password])
       session[:user_id] = user.id.to_s
       redirect_to posts_path
     else
