f6b3a2b57908bb11a21ee96bb709c7a9703437c4
Michael Cruz <michael.cruz@uwalumni.com>
2016-10-30 22:55:25 -0700

    adds password confirmation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:login][:email])
-    if user && user.authenticate(params[:login][:password])
+    if user && user.authenticate(params[:login][:password][:password_confirmation])
       session[:user_id] = user.id.to_s
       redirect_to posts_path
     else
