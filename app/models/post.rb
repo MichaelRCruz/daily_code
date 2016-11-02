a207b6caa5514c3d52c69c887c855644f24eef35
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-02 15:17:22 -0700

    restricts file upload type

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_attached_file :snippet
   has_many :comments, dependent: :destroy
 
-  # validates_attachment_content_type :snippet, :content_type => ["*/*"]
+  validates_attachment_content_type :snippet, :content_type => ['text/plain']
 
-  do_not_validate_attachment_file_type :snippet
+  # do_not_validate_attachment_file_type :snippet
 end
