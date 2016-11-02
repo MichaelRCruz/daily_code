ed8312e7330db23d8d552873d8a80ec569c2097f
Michael Cruz <michael.cruz@uwalumni.com>
2016-11-02 16:46:55 -0700

    creates cli tool

--- cli.rb
+++ cli.rb
@@ -3,19 +3,27 @@ require 'uri'
 require 'net/http'
 
 user_input = ARGV
+user_email = 'test'
+user_password = 'test'
 
-params = {
-  'email' => user_input[0],
-  'password' => user_input[1],
-  'title' => user_input[2],
-  'content' => user_input[3],
-  'language' => user_input[4],
-  'snippet' => open(user_input[5]).read
-}
+if ARGV[0] == user_email && ARGV[1] == user_password
 
-json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
+  params = {
+    'email' => user_input[0],
+    'password' => user_input[1],
+    'title' => user_input[2],
+    'content' => user_input[3],
+    'language' => user_input[4],
+    'snippet' => open(user_input[5]).read
+  }
 
-uri = URI.parse('http://localhost:3000/posts')
-http = Net::HTTP.new(uri.host, uri.port)
+  json_headers = {"Content-Type" => "application/json", "Accept" => "application/json"}
 
-response = http.post(uri.path, params.to_json, json_headers)
+  uri = URI.parse('http://dailycode.herokuapp.com/posts')
+  http = Net::HTTP.new(uri.host, uri.port)
+
+  response = http.post(uri.path, params.to_json, json_headers)
+  puts response
+else
+  puts "incorrect email and/or password"
+end
